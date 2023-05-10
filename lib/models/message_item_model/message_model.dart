4ce792c1058275064621afdacbdbdece183ffae9
Nomanshariq562 <86268414+Nomanshariq562@users.noreply.github.com>
2023-05-10 16:01:39 +0500

    icon changed

--- lib/models/message_item_model/message_model.dart
+++ lib/models/message_item_model/message_model.dart
@@ -1,10 +1,13 @@
 class MessageItemModel {
-
   String name;
   String mostRecentMessage;
   String messageDate;
   String type;
 
-  MessageItemModel(this.name, this.mostRecentMessage, this.messageDate, this.type);
-
+  MessageItemModel(
+    this.name,
+    this.mostRecentMessage,
+    this.messageDate,
+    this.type,
+  );
 }
